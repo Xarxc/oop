46aecc775b14b2621a16464dd114d5712e483b93
Mohamed ahmed <mohamed20091004@outlook.com>
2023-09-23 13:53:51 +0100

    First Commit

---
+++ object.js
@@ -0,0 +1,16 @@
+ 
+let player1 = {
+ firstName : "Gashaan",
+ lastname : "Adam",
+ age : 13
+}
+
+let player2 = { 
+ firstName : "Younis",
+ lastname : "Yusuf",
+ age : 11
+}
+
+console.log(player1.firstName)
+console.log(player2.firstName)
+
